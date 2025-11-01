3c2c31410d7f0dbbcaa9472e456b9de55eb98fee
EmersonDHH <ehidalgoh@ucenfotec.ac.cr>
2025-10-31 19:16:42 -0600

    Landing Autocut Translated

--- autocut/src/app/app.config.ts
+++ autocut/src/app/app.config.ts
@@ -2,13 +2,16 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {HttpClient,provideHttpClient, withInterceptors,} from '@angular/common/http';
 import { baseUrlInterceptor } from './core/interceptors/base-url.interceptor';
 import { accessTokenInterceptor } from './core/interceptors/access-token.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { APP_INITIALIZER } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { handleErrorsInterceptor } from '././core/interceptors/handle-errors.interceptor';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { CustomTranslateLoader } from './core/utils/custom-translate-loader';
 
 export function initializeAuth(authService: AuthService): () => Promise<void> {
   return () => authService.init();
@@ -32,5 +35,14 @@ export const appConfig: ApplicationConfig = {
       deps: [AuthService],
       multi: true,
     },
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useClass: CustomTranslateLoader,
+        },
+      })
+    ),
   ],
 };
--- autocut/src/app/pages/features/landing-autocut/components/landing-footer/landing-footer.component.ts
+++ autocut/src/app/pages/features/landing-autocut/components/landing-footer/landing-footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './landing-footer.component.html',
 })
 export class LandingFooterComponent {
